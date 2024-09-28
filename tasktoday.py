d72b86908fa31159621688fc35ac9c08c6408b17
Ganztech <ganz000005@gmail.com>
2024-09-28 11:32:46 +0800

    akoranitisoy

---
+++ tasktoday.py
@@ -0,0 +1,13 @@
+age=[-12,13,-14,15,-16]
+
+for data in age:
+    if data > 0:
+        print(str(data)+ "negative")
+    elif data > 0:
+        print(str(data)+ "positive")
+
+for dataUser in isername:
+    if dataUser == "james":
+        print("james cute")
+    else:
+        print("james none")
